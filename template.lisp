42310cb4222c021f5031b0a8db89bd2cb45e4b38
ArgentCorvid <khokanson@gmail.com>
2025-12-01 07:12:18 -0600

    Advent of Code 2025 Initial commit

---
+++ template.lisp
@@ -0,0 +1,35 @@
+;;;2025 day x
+
+(eval-when (:compile-toplevel :load-toplevel)
+  (ql:quickload '(:alexandria :str))
+  (add-package-local-nickname 'a 'alexandria-2))
+
+(defparameter *day-number* x)
+(defparameter *input-name-template* "2025d~dinput.txt")
+
+(defparameter *test-input*
+  '())
+
+(defun parse-input (lines)
+  )
+
+(defun p1 ()
+  ) 
+
+(defun p2 ()
+  )
+
+(defun run (parts-list data)
+  (dolist (part (a:ensure-list parts-list))
+    (ccase part
+      (1 (format t "~&Part 1: ~a" (p1 data)))
+      (2 (format t "~&Part 2: ~a" (p2 data))))))
+
+(defun main (&rest parts)
+  (let* ((infile-name (format nil *input-name-template* *day-number*))
+         (input-lines (uiop:read-file-lines infile-name))
+         (data (parse-input input-lines)))
+    (run parts data)))
+
+(defun test (&rest parts)
+  (run parts (parse-input *test-input*)))
