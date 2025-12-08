7b3275166ef51a504047ebcef6118e05654b53ea
ArgentCorvid <khokanson@gmail.com>
2025-12-08 12:41:09 -0600

    2025 d6 p2 working on parsing.
    now have list of lists of characters for each grouping

--- 2025d6.lisp
+++ 2025d6.lisp
@@ -21,17 +21,28 @@
   "print to standard output only if *verbose* is true"
   (apply #'format t format-string args))
 
-(defun parse-input (lines)
+(defun parse-input (lines &key (part 1))
   (flet ((parse-int-but-op (line-in)
            (let ((split-line (str:split-omit-nulls #\space line-in)))
              (if  (find (elt split-line 0) '("+" "*") :test #'equal )
                   (mapcar #'a:ensure-symbol split-line)
                   (mapcar #'parse-integer split-line)))))
-    (a:line-up-last
-     lines
-     (mapcar #'parse-int-but-op)
-     (a:rotate)
-     (apply #'mapcar #'list))))
+    (case part
+      (1 (a:line-up-last
+          lines
+          (mapcar #'parse-int-but-op)
+          (a:rotate)
+          (apply #'mapcar #'list)))
+      (2  ;;all lines 3747 long, max width each group 4
+       ;; all operator chars on lsd column
+       (let ((numbers-to-operate (list)))
+         (ppcre:do-matches (left-posn sep-posn "[+*]\\s+" (a:lastcar lines) (list numbers-to-operate))
+           ;; each "problem grouping"
+           (push (loop :with right-posn = (1- sep-posn)
+                       :for pos :from right-posn :downto left-posn
+                       :collect (loop :for l :in lines
+                                      :collect (aref l pos)))
+                 numbers-to-operate)))))))
 
 (defun p1 (operations)
   (a:line-up-last
@@ -42,18 +53,18 @@
 (defun p2 ()
   )
 
-(defun run (parts-list data)
+(defun run (parts-list input-lines)
   (dolist (part (a:ensure-list parts-list))
-    (ccase part
-      (1 (format t "~&Part 1: ~a" (p1 data)))
-      (2 (format t "~&Part 2: ~a" (p2 data))))))
+    (let ((data (parse-input input-lines :part part)))
+      (ccase part
+        (1 (format t "~&Part 1: ~a" (p1 data)))
+        (2 (format t "~&Part 2: ~a" (p2 data)))))))
 
 (defun main (&rest parts)
   (let* ((infile-name (format nil *input-name-template* *day-number*))
-         (input-lines (uiop:read-file-lines infile-name))
-         (data (parse-input input-lines)))
-    (run parts data)))
+         (input-lines (uiop:read-file-lines infile-name)))
+    (run parts input-lines)))
 
 (defun test (&rest parts)
   (let ((*verbose* t))
-    (run parts (parse-input *test-input*))))
+    (run parts *test-input*)))
