3095e94194888fddc853fbf800b1cf665533c71c
ArgentCorvid <khokanson@gmail.com>
2025-12-14 17:00:32 -0600

    2025 d8 p1 works!

--- 2025d8.lisp
+++ 2025d8.lisp
@@ -39,6 +39,11 @@
   (when *verbose*
     (apply #'format t format-string args)))
 
+(declaim (inline equal-intersection
+                                  equal-union
+                                  pair-distance
+                                  point-distance))
+
 (defun point-distance (p1 p2)
   (isqrt (loop for c1 fixnum in p1
                and c2 fixnum in p2
@@ -61,59 +66,56 @@
 (defun equal-union (list1 list2 &rest args)
   (apply #'union list1 list2 :test #'equal args))
 
-(defun connect-pair (j-box-pair)
-  "move pair of junction boxes to the appropriate circuit"
-  (declare (special circuits))
-  (destructuring-bind (box1 box2) j-box-pair)
-  
 
-  (setf circuits ()))
+
+(defun join-circuits (c1 c2 circuits-in)
+  (let ((new-circuit (equal-union c1 c2)))
+    (cons new-circuit (remove new-circuit circuits-in :test #'equal-intersection))))
 
 (defun p1 (j-boxes-in num-to-connect &optional ( number-of-circuits 3))
   (let ((pairs (list))
-        (circuits (list)))
-    (declare (special circuits))
+        (circuits (mapcar #'list j-boxes-in)))
     (a:map-combinations (lambda (point-pair)
                           (push point-pair pairs))
                         j-boxes-in
                         :length 2)
     (sortf pairs #'< :key #'pair-distance)
     (vformat "~&10 closest pairs:~& ~a" (subseq pairs 0 10))
-    (push (first pairs) circuits) ;first pair is definitely a circuit
     (mapc (lambda (pair)
-            (a:if-let ((it (find pair circuits ; if found in circuits, union circuit and pair, replace? else, push pair to circuits?
-                                 :test #'equal-intersection)))
-              (progn (vformat "~&found ~a in circuits~% for pair ~a" it pair)
-                     (setf circuits (remove-duplicates (substitute (equal-union it pair)
-                                                                   it
-                                                                   circuits
-                                                                   :test #'equal-intersection)
-                                                       :test #'a:set-equal)))  
-              (progn (vformat "~&no match for pair ~a" pair)
-                     (push pair circuits)))
+            (destructuring-bind (c1 c2)
+                (loop :for point :in pair
+                      :collect (find (list point) circuits
+                                     :test #'equal-intersection))
+              
+              (if (or c1 c2)
+                  (progn (vformat "~&found ~@[~a  ~]~@[~a ~]in circuits~% for pair ~a" c1 c2 pair)
+                         (setf circuits (join-circuits  c1 c2 circuits)))  
+                  (progn (vformat "~&no match for pair ~a" pair))))
             (vformat "~&  circuits now: ~a" circuits))
-          (subseq pairs 1 num-to-connect))
-    ;; (break)
-    ;; (reduce #'* (sort circuits #'> :key #'length)
-    ;;         :key #'length :end number-of-circuits :initial-value 1)
-    (sort circuits #'> :key #'length)
-    )) 
+          (subseq pairs 0 num-to-connect))
+  
+    (reduce #'* (sort (mapcar #'length circuits) #'>)
+            :end number-of-circuits :initial-value 1))) 
 
 (defun p2 ()
   )
 
 (defun run (parts-list data)
-  (dolist (part (a:ensure-list parts-list))
-    (ccase part
-      (1 (format t "~&Part 1: ~a" (p1 data)))
-      (2 (format t "~&Part 2: ~a" (p2 data))))))
+  )
 
 (defun main (&rest parts)
   (let* ((infile-name (format nil *input-name-template* *day-number*))
          (input-lines (uiop:read-file-lines infile-name))
          (data (parse-input input-lines)))
-    (run parts data)))
+    (dolist (part (a:ensure-list parts))
+      (ccase part
+        (1 (format t "~&Part 1: ~a" (p1 data 1000)))
+        (2 (format t "~&Part 2: ~a" (p2 data)))))))
 
 (defun test (&rest parts)
-  (let ((*verbose* t))
-    (run parts (parse-input *test-input*))))
+  (let ((*verbose* t)
+        (data (parse-input *test-input*)))
+    (dolist (part (a:ensure-list parts))
+    (ccase part
+      (1 (format t "~&Part 1: ~a" (p1 data 10)))
+      (2 (format t "~&Part 2: ~a" (p2 data)))))))
